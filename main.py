73124e03f7ece65b2756b68d214bca84f4dcb1d9
Thiago Ajeje <63625421+thiajeje@users.noreply.github.com>
2022-11-23 23:57:02 +0000

    Fizemos um exemplo simples de orientação a objetos

---
+++ main.py
@@ -0,0 +1,9 @@
+# Importa nosso arquivo Pessoa.py no diretório model
+from model.Pessoa import Pessoa
+
+# Exemplo de uso
+poyatos = Pessoa(1, "Henrique Poyatos")
+print(poyatos)
+
+# Quero mostrar só o nome
+print(poyatos.nome)
